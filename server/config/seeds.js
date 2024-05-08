108fa7b990fb7b6423f46dfca0c46d806e810150
Amartin02 <Tankfurnace641@gmail.com>
2024-05-08 13:48:16 -0600

    added seed data

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,153 +1,60 @@
 const db = require("./connection");
-const { User, Item, UserOrder, Cart } = require("../models");
+const { User, Item } = require("../models");
 
 db.once("open", async () => {
-  await cleanDB("Category", "categories");
-  await cleanDB("Product", "products");
-  await cleanDB("User", "users");
-
-  const categories = await Category.insertMany([
-    { name: "Food" },
-    { name: "Household Supplies" },
-    { name: "Electronics" },
-    { name: "Books" },
-    { name: "Toys" },
+  const users = await User.insertMany([
+    { name: "Ben", email: "ben2332@gmail.com", password: "supersecret" },
+    {
+      name: "Phyllis",
+      email: "phyllis5632@gmail.com",
+      password: "supersecret",
+    },
+    {
+      name: "Victor",
+      email: "victor3256@getMaxListeners.com",
+      password: "supersecret",
+    },
+    {
+      name: "Anthony",
+      email: "tankjhkf541@gmail.com",
+      password: "supersecret",
+    },
   ]);
 
-  console.log("categories seeded");
+  console.log("Users seeded");
 
-  // const products = await Product.insertMany([
-  //   {
-  //     name: 'Tin of Cookies',
-  //     description:
-  //       'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-  //     image: 'cookie-tin.jpg',
-  //     category: categories[0]._id,
-  //     price: 2.99,
-  //     quantity: 500
-  //   },
-  //   {
-  //     name: 'Canned Coffee',
-  //     description:
-  //       'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-  //     image: 'canned-coffee.jpg',
-  //     category: categories[0]._id,
-  //     price: 1.99,
-  //     quantity: 500
-  //   },
-  //   {
-  //     name: 'Toilet Paper',
-  //     category: categories[1]._id,
-  //     description:
-  //       'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-  //     image: 'toilet-paper.jpg',
-  //     price: 7.99,
-  //     quantity: 20
-  //   },
-  //   {
-  //     name: 'Handmade Soap',
-  //     category: categories[1]._id,
-  //     description:
-  //       'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-  //     image: 'soap.jpg',
-  //     price: 3.99,
-  //     quantity: 50
-  //   },
-  //   {
-  //     name: 'Set of Wooden Spoons',
-  //     category: categories[1]._id,
-  //     description:
-  //       'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-  //     image: 'wooden-spoons.jpg',
-  //     price: 14.99,
-  //     quantity: 100
-  //   },
-  //   {
-  //     name: 'Camera',
-  //     category: categories[2]._id,
-  //     description:
-  //       'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-  //     image: 'camera.jpg',
-  //     price: 399.99,
-  //     quantity: 30
-  //   },
-  //   {
-  //     name: 'Tablet',
-  //     category: categories[2]._id,
-  //     description:
-  //       'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-  //     image: 'tablet.jpg',
-  //     price: 199.99,
-  //     quantity: 30
-  //   },
-  //   {
-  //     name: 'Tales at Bedtime',
-  //     category: categories[3]._id,
-  //     description:
-  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-  //     image: 'bedtime-book.jpg',
-  //     price: 9.99,
-  //     quantity: 100
-  //   },
-  //   {
-  //     name: 'Spinning Top',
-  //     category: categories[4]._id,
-  //     description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-  //     image: 'spinning-top.jpg',
-  //     price: 1.99,
-  //     quantity: 1000
-  //   },
-  //   {
-  //     name: 'Set of Plastic Horses',
-  //     category: categories[4]._id,
-  //     description:
-  //       'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-  //     image: 'plastic-horses.jpg',
-  //     price: 2.99,
-  //     quantity: 1000
-  //   },
-  //   {
-  //     name: 'Teddy Bear',
-  //     category: categories[4]._id,
-  //     description:
-  //       'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-  //     image: 'teddy-bear.jpg',
-  //     price: 7.99,
-  //     quantity: 100
-  //   },
-  //   {
-  //     name: 'Alphabet Blocks',
-  //     category: categories[4]._id,
-  //     description:
-  //       'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-  //     image: 'alphabet-blocks.jpg',
-  //     price: 9.99,
-  //     quantity: 600
-  //   }
-  // ]);
+  const items = await Item.insertMany([
+    {
+      name: "RUST FERRIS T-SHIRT",
+      description:
+        "100% cotton Rib-Knit Crew Neck Double needle sleeves and hem",
+      category: "shirt",
+      price: 20.0,
+    },
+    {
+      name: "EMBER MASCOT STICKERS",
+      description:
+        "Meet the friendly neighborhood Tomster. We love him, and you should too... ",
+      category: "sticker",
+      price: 6.0,
+    },
+    {
+      name: "MSI Codex R2 C13NUC5-218US Gaming PC",
+      description:
+        "Intel Core i5 13400F (1.8GHz) Processor 16GB DDR5-5600 RAM NVIDIA GeForce RTX 4060 Graphics Card1TB M.2 NVMe Gen4 SSD 2.5GbE LAN, WiFi 7 (802.11be), Bluetooth 5.4",
+      category: "computer",
+      price: 1999.99,
+    },
+    {
+      name: "RUST FERRIS MUG",
+      description:
+        "Impress your coffee dates with this sleek, glossy, Rust Ferris mug! It's the perfect way to show off your caffeinated sophistication.",
+      category: "mug",
+      price: 12.0,
+    },
+  ]);
 
-  // console.log('products seeded');
-
-  // await User.create({
-  //   firstName: 'Pamela',
-  //   lastName: 'Washington',
-  //   email: 'pamela@testmail.com',
-  //   password: 'password12345',
-  //   orders: [
-  //     {
-  //       products: [products[0]._id, products[0]._id, products[1]._id]
-  //     }
-  //   ]
-  // });
-
-  // await User.create({
-  //   firstName: 'Elijah',
-  //   lastName: 'Holt',
-  //   email: 'eholt@testmail.com',
-  //   password: 'password12345'
-  // });
-
-  // console.log('users seeded');
+  console.log("Items seeded");
 
   process.exit();
 });
