eda60df9eee3c6b9aa5527f776d1d3c7299ec705
GiorDior <giordant@yahoo.com>
2024-05-09 10:16:14 -0600

    Card React Element created

---
+++ client/src/components/Carousel/index.jsx
@@ -0,0 +1,38 @@
+import { Carousel } from "react-bootstrap";
+// import Image1 from "../images";
+import Placeholder from "react-bootstrap/Placeholder";
+
+// import { Carousel } from "react-bootstrap/Carousel";
+// import Image1 from "../images/Image1";
+
+function CarouselFadeExample() {
+  return (
+    <Carousel fade>
+      <Carousel.Item>
+        <Carousel.img src="holder.js/100px180" text="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Carousel.img src="holder.js/100px180" text="Second slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Carousel.img src="holder.js/100px180" text="Third slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default CarouselFadeExample;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,47 @@
+import ProductCard from "../components/Card";
+import CarouselFadeExample from "../components/Carousel";
+// import ExampleCarouselImage from "../assets/images"
+
 const Home = () => {
-  return <div className="container"></div>;
+  const products = [
+    {
+      //link, image, title, text
+      link: "/products/1",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/7/7a/Basketball.png",
+      title: "Basketball",
+      text: "This is a good basketball",
+    },
+    {
+      //link, image, title, text
+      link: "/products/2",
+      image:
+        // "../assets/images/ExampleCarouselImage.svg"
+        "https://m.media-amazon.com/images/I/51-ruKlRJRL._AC_UF1000,1000_QL80_.jpg",
+      title: "Volleyball",
+      text: "This is a good volleyball",
+    },
+  ];
+
+  return (
+    //attach carousel here
+
+    <div className="container d-flex justify-content-around">
+      {products ? (
+        products.map((product) => (
+          <ProductCard
+            link={product.link}
+            image={product.image}
+            title={product.title}
+            text={product.text}
+            key={product.link}
+          />
+        ))
+      ) : (
+        <div>loading... </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
